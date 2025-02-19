6aceeeb9ec7bef49c40ab2d6e9145f94a0201c8a
Caden <cadenzach@nanonation.net>
2025-02-19 11:32:03 -0600

    More lottery stuff

--- Ups & Downs/GameLogic/Lottery.cs
+++ Ups & Downs/GameLogic/Lottery.cs
@@ -5,20 +5,39 @@ namespace UpsAndDowns.GameLogic;
 public class Lottery
 {
 	public int WinningsCashAmount { get; private set; };
+	public List<string> LotteryTickets = new();
 
 	private Random _random = new();
 
 	public Lottery()
 	{
-
+		SetLotteryWinnings();
 	}
 
 	public void SetLotteryWinnings()
 	{
+		// Randomly select a winnings amount  between 10,000 and 100,000
 		int amount = 10000 * _random.Next(1, 11)
 		WinningsCashAmount = amount;
 	}
 
+	public void GenerateLotteryTickets(int players)
+	{
+		LotteryTickets.Clear();
+		for (int i = 0; i < players; i++)
+		{
+			LotteryTickets.Add(GenerateRandom4DigitNumber().ToString());
+		}
+	}
+
+	public int GenerateRandom4DigitNumber()
+	{
+		// Generates a number between 1000 and 9999
+		int number = _random.Next(1000, 10000); 
+		return number;
+	}
+
+	// TODO: Change this to select a winner from the list of tickets
 	public LotteryResult Draw(List<Player> players, out Player winner)
 	{
 		winner = null;
