87a344d58f4794de3a18c3b860cfa266aa2227e2
Caden <cvfxz16@gmail.com>
2025-02-22 16:49:07 -0600

    Add button click bindings for player turn UI

--- Ups & Downs/Controls/PlayerTurnScreen.xaml.cs
+++ Ups & Downs/Controls/PlayerTurnScreen.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using UpsAndDowns.GameLogic;
@@ -28,7 +29,63 @@ namespace UpsAndDowns.Controls
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
+            Debug.WriteLine("BackButton_Click");
             WeakReferenceMessenger.Default.Send(new Messages.ReturnToHomeScreenMessage());
+        }
+
+        private void SpaceButton_Green(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpaceButton_Green");
+        }
+
+        private void SpaceButton_Blue(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpaceButton_Blue");
+        }
+
+        private void SpaceButton_Yellow(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpaceButton_Yellow");
+        }
+
+        private void SpaceButton_Pink(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpaceButton_Pink");
+        }
+
+        private void SpecialButton_Education(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpecialButton_Education");
+        }
+
+        private void SpecialButton_Promotion(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpecialButton_Promotion");
+        }
+
+        private void SpecialButton_Marriage(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpecialButton_Marriage");
+        }
+
+        private void SpecialButton_Children(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpecialButton_Children");
+        }
+
+        private void SpecialButton_SpecialEvent(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpecialButton_SpecialEvent");
+        }
+
+        private void SpecialButton_BuyCar(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpecialButton_BuyCar");
+        }
+
+        private void SpecialButton_BuyHouse(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("SpecialButton_BuyHouse");
         }
     }
 }
