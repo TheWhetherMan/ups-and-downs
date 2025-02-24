0703da106e5f9f0ef02d728954fe2aba0236353b
Caden <cvfxz16@gmail.com>
2025-02-23 22:04:01 -0600

    Serilog stuff. Changing resolution values

--- Ups & Downs/BusinessLogic/Logger.cs
+++ Ups & Downs/BusinessLogic/Logger.cs
@@ -4,18 +4,28 @@ namespace UpsAndDowns.BusinessLogic;
 
 public static class Logger
 {
+    private static Serilog.Core.Logger _logger = new LoggerConfiguration()
+        .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+    static Logger()
+    {
+        _logger = new LoggerConfiguration()
+            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+    }
+
     public static void Log(string message, object? context = null)
     {
         if (context is null)
         {
             Console.WriteLine($"{DateTime.Now} :: {message}");
-            Serilog.Log.Information(message);
+            _logger.Information(message);
             return;
         }
         else
         {
             Console.WriteLine($"{DateTime.Now} :: {message} :: {context}");
-            Serilog.Log.Information("{Message}, {Context}", message, context);
+            _logger.Information("{Message}, {Context}", message, context);
         }
     }
 }
